c9968318378c0fa1b5c459da0c15fbd59b4a9c0e
WillmanGZ <willmang@uninorte.edu.co>
2025-12-22 18:38:32 -0500

    feat: break cards down in components

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
@@ -7,6 +8,8 @@ export const SearchPage = () => {
         title="Búsqueda de Super Héroes"
         description="Descubre, explora y administra super héroes y villanos"
       />
+
+      <HeroStats />
     </>
   );
 };
