a50df49872150e50841b9518c2f43841c8ec342e
WillmanGZ <willmang@uninorte.edu.co>
2025-12-22 18:00:28 -0500

    feat: implement lazy load

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,10 +1,11 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 import { HomePage } from "../heroes/pages/home/HomePage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { AdminLayout } from "@/admin/pages/layouts/AdminLayout";
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
